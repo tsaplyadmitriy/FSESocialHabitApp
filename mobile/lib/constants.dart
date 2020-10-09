666d95d8f6446462115c8451e3cad3664347d59f
Gleb <gleb.osotov@gmail.com>
2020-10-09 17:43:45 +0300

    before redoing card design

--- mobile/lib/constants.dart
+++ mobile/lib/constants.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-
 class constants {
   static const kPrimaryColor = Color(0xFF6F35A5);
   static const kPrimaryLightColor = Color(0xFFF1E6FF);
+  static const kSecondaryColor = Color(0xff3d315b);
+  static const anotherColor = Color(0xff3d315b);
   static Color backgroundColor = Color.fromARGB(100, 242, 242, 242);
   static LinearGradient gradient() {
-    return LinearGradient(
-        begin: Alignment.centerLeft,
-        end: Alignment.centerRight,
-        colors: [Color.fromARGB(255, 111, 53, 165), Color.fromARGB(255,161, 114, 204), Color.fromARGB(255, 241, 230, 255)],
-        stops: [0.25, 1, 1]);
+    return LinearGradient(colors: [
+      anotherColor,
+      kSecondaryColor,
+    ], begin: Alignment.topLeft, end: Alignment.bottomRight);
   }
 }
--- mobile/lib/screens/findgroup/group_card_page.dart
+++ mobile/lib/screens/findgroup/group_card_page.dart
@@ -1,56 +1,35 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:social_habit_app/constants.dart';
 import 'package:social_habit_app/group.dart';
 
-
-
-
-
-
 class GroupCardDialog extends StatelessWidget {
   Group entity;
 
-
   GroupCardDialog(Group entity) {
     this.entity = entity;
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
-
       shape: RoundedRectangleBorder(
-
         borderRadius: BorderRadius.circular(10),
       ),
       elevation: 0,
-
-
       backgroundColor: Colors.transparent,
-
-
-          child:dialogContent(context),
+      child: dialogContent(context),
     );
   }
 
-
   dialogContent(BuildContext context) {
-    print("test "+entity.preferences.length.toString());
+    Size size = MediaQuery.of(context).size; // h and w of s
+    //print("test "+entity.preferences.length.toString());
     return Stack(
-
       children: <Widget>[
         Container(
-
-          padding: EdgeInsets.only(
-            top: 20,
-            bottom: 20,
-            left: 15,
-            right: 15,
-          ),
-          margin: EdgeInsets.only(top: 10,left: 15,right: 15),
+          padding: EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+          margin: EdgeInsets.symmetric(vertical: 40, horizontal: 15),
           decoration: new BoxDecoration(
             boxShadow: [
               BoxShadow(
@@ -62,84 +41,77 @@ class GroupCardDialog extends StatelessWidget {
             color: Colors.white,
             shape: BoxShape.rectangle,
             borderRadius: BorderRadius.circular(15),
-
           ),
           child: Column(
-
             //mainAxisSize: MainAxisSize.min, // To make the card compact
             children: <Widget>[
-
               Text(
                 entity.groupName,
                 style: TextStyle(
                   fontSize: 24.0,
-                  fontWeight: FontWeight.w700,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
               Row(
-
-
-                  children:
-                  entity.tags.map((String string) {
-                    return Container(
-                        margin: EdgeInsets.only(right:5 ),
-
-                        decoration: BoxDecoration(gradient: constants.gradient(),borderRadius:BorderRadius.circular(10.0) ),
-
-                        child:
-                        Card(
-
-
-                            margin: EdgeInsets.only(bottom: 3, top: 3, right: 8, left: 8),
-                            color: Colors.transparent,
-                            elevation: 0,
-                            child:
-                            Container(
-                              // decoration: BoxDecoration(gradient: constants.gradient()),
-                              child:
-                              Text(string,style: TextStyle(fontSize: 12, fontWeight:FontWeight.w400 ,color: Colors.white),),
-                            )
-                        ));
-
-                  }).toList()
-              ),
+                  children: entity.tags.map((String string) {
+                return Container(
+                    margin: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                    decoration: BoxDecoration(
+                        gradient: constants.gradient(),
+                        borderRadius: BorderRadius.circular(15.0)),
+                    child: Card(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                        color: Colors.transparent,
+                        elevation: 5,
+                        child: Container(
+                          // decoration: BoxDecoration(gradient: constants.gradient()),
+                          child: Text(
+                            string,
+                            style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          ),
+                        )));
+              }).toList()),
               SizedBox(height: 16.0),
-              Row(children: entity.preferences.map((String pref) {
-                return Text("#"+pref+" ",style: TextStyle(fontStyle: FontStyle.italic,fontSize: 12, fontWeight:FontWeight.w300),);
-
-              }).toList(),),
+              Row(
+                children: entity.preferences.map((String pref) {
+                  return Text(
+                    "#" + pref + "\t",
+                    style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400),
+                  );
+                }).toList(),
+              ),
               Container(
-                  margin: EdgeInsets.only(left: 10,right: 10),
-                  child:Text(
-
-                entity.groupDescription,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              )),
-
-              SizedBox(height: 124.0),
-
-
-
-
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                  child: Text(
+                    entity.groupDescription,
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  )),
+              SizedBox(height: size.height * 0.05),
               Align(
                 alignment: Alignment.bottomCenter,
-                child:FlatButton(
-
+                child: FlatButton(
                   // borderSide: BorderSide(color: Constants.primary, width: 2),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)
-                  ),
-                  child:
-                  Ink(
-
-                      decoration: BoxDecoration(gradient: constants.gradient(), borderRadius: BorderRadius.circular(30.0)),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Ink(
+                      decoration: BoxDecoration(
+                          gradient: constants.gradient(),
+                          borderRadius: BorderRadius.circular(30.0)),
                       child: Container(
-                          constraints: BoxConstraints(maxWidth: 200.0, minHeight: 50.0),
+                          constraints:
+                              BoxConstraints(maxWidth: 200.0, minHeight: 50.0),
                           alignment: Alignment.center,
-                          child:
-                          Text(
+                          child: Text(
                             "Apply",
                             style: TextStyle(color: Colors.white),
                           ))),
@@ -148,7 +120,6 @@ class GroupCardDialog extends StatelessWidget {
                   },
                 ),
               ),
-
             ],
           ),
         ),
